fc8ccead328accb6c1ccc7d15463fcd30ac8e396
Adrian Rosicki <adrian.rosicki.9@gmail.com>
2022-01-02 00:55:15 +0100

    Setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 (async () => {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle("News Aggregator")
+    .setDescription("News Aggregator API")
+    .setVersion("1.0")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
   await app.listen(3000);
 })();
