aa35020606f801e10bb52931b1f31be82b631708
Adrian Rosicki <adrian.rosicki.9@gmail.com>
2022-01-01 23:01:53 +0100

    Setup global validation  pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
-async function bootstrap() {
+(async () => {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
-}
-bootstrap();
+})();
