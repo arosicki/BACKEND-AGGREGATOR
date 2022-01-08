677aa6d505d10cb43b5b27f44ee470be95933b1e
Adrian Rosicki <adrian.rosicki.9@gmail.com>
2022-01-08 03:59:37 +0100

    remove unused import

--- src/settings/dto/update-setting.dto.ts
+++ src/settings/dto/update-setting.dto.ts
@@ -1,10 +1,9 @@
-import { IsInt, IsPositive, Min } from "class-validator";
+import { IsInt, IsPositive } from "class-validator";
 import { ApiProperty } from "@nestjs/swagger";
 
 export class UpdateSettingDto {
   @IsInt()
   @IsPositive()
-  @Min(1000 * 60 * 5) // 5mins
   @ApiProperty({ type: "integer", description: "key value", example: 720000 })
   value: number;
 }
