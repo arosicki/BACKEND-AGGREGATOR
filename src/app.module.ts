df965f30758c00e93d86040c5a771c3692040c4f
Adrian Rosicki <adrian.rosicki.9@gmail.com>
2022-01-09 23:07:08 +0100

    generate auth resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from "./app.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ScheduleModule } from "@nestjs/schedule";
 import { SettingsModule } from "./settings/settings.module";
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ScheduleModule.forRoot(), TypeOrmModule.forRoot(), SettingsModule],
+  imports: [ScheduleModule.forRoot(), TypeOrmModule.forRoot(), SettingsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
