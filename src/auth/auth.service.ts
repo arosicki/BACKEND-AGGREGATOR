9dc1d8433ee95f4769c082e96277455c1200469d
Adrian Rosicki <adrian.rosicki.9@gmail.com>
2022-01-10 20:06:54 +0100

    create service methods

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { SignInLocalDto, SignUpLocalDto } from "./dto";
+import { User } from "./entities/user.entity";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
+  ) {}
+  async signUpLocal(_signUpLocalDto: SignUpLocalDto) {
+    this.userRepository; // prevent unused vars ts err
+  }
+  async signInLocal(_signInLocalDto: SignInLocalDto) {}
+  async logout() {}
+  async refreshToken() {}
+}
