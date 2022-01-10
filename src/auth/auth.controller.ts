55c0f98c52f0d7ee2c889771cbdbc9dc6ccae426
Adrian Rosicki <adrian.rosicki.9@gmail.com>
2022-01-10 20:05:39 +0100

    create controller routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { ApiTags } from "@nestjs/swagger";
 import { AuthService } from "./auth.service";
+import { SignInLocalDto, SignUpLocalDto } from "./dto";
 
 @Controller("auth")
+@ApiTags("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post()
-  async signUpLocal() {
-    this.authService;
+  @Post("local/signup")
+  async signUpLocal(@Body() signUpLocalDto: SignUpLocalDto) {
+    this.authService.signUpLocal(signUpLocalDto);
+  }
+  @Post("local/signin")
+  async signInLocal(@Body() signInLocalDto: SignInLocalDto) {
+    this.authService.signInLocal(signInLocalDto);
+  }
+  @Post("logout")
+  async logout() {
+    this.authService.logout();
+  }
+  @Post("refresh")
+  async refreshToken() {
+    this.authService.refreshToken();
   }
 }
