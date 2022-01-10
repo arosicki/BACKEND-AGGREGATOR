142b1a5b2f3ba73c9bc7c581efb6adade9e33409
Adrian Rosicki <adrian.rosicki.9@gmail.com>
2022-01-10 19:24:09 +0100

    add IsUsernameAlreadyTaken constraint to dto

--- src/auth/dto/sign-up-local.dto.ts
+++ src/auth/dto/sign-up-local.dto.ts
@@ -1,10 +1,12 @@
 import { IsOptional, IsString, Matches, MaxLength, MinLength } from "class-validator";
 import { passwordRequirementsConfig } from "src/config/password-requirements-config";
+import { IsUsernameAlreadyTaken } from "../decorators";
 
 export class signUpLocalDto {
   @IsString()
   @MinLength(6)
   @MaxLength(32)
+  @IsUsernameAlreadyTaken()
   username: string;
   @IsString()
   @Matches(passwordRequirementsConfig.getRegex(), { message: passwordRequirementsConfig.getDescription() })
