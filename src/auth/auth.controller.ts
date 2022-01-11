b6df7d7d84e331dfd27cd0ad40e4dd25bc039c8b
Adrian Rosicki <adrian.rosicki.9@gmail.com>
2022-01-11 16:52:40 +0100

    implement signup route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,9 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
+import { CreatedResponseObject } from "src/utils/response-object";
 import { AuthService } from "./auth.service";
 import { SignInLocalDto, SignUpLocalDto } from "./dto";
-
+import { TokensInterface } from "./interfaces";
 @Controller("auth")
 @ApiTags("auth")
 export class AuthController {
@@ -10,7 +11,8 @@ export class AuthController {
 
   @Post("local/signup")
   async signUpLocal(@Body() signUpLocalDto: SignUpLocalDto) {
-    this.authService.signUpLocal(signUpLocalDto);
+    const tokens = await this.authService.signUpLocal(signUpLocalDto);
+    return new CreatedResponseObject<TokensInterface>("account succesfully created", tokens);
   }
   @Post("local/signin")
   async signInLocal(@Body() signInLocalDto: SignInLocalDto) {
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,74 @@
 import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { JwtService } from "@nestjs/jwt";
 import { InjectRepository } from "@nestjs/typeorm";
+import { hash } from "argon2";
 import { Repository } from "typeorm";
 import { SignInLocalDto, SignUpLocalDto } from "./dto";
-import { User } from "./entities/user.entity";
+import { Token, User, UserTypes } from "./entities";
+import { TokensInterface } from "./interfaces";
 
 @Injectable()
 export class AuthService {
   constructor(
+    private readonly jwtService: JwtService,
     @InjectRepository(User)
-    private readonly userRepository: Repository<User>
+    private readonly userRepository: Repository<User>,
+    @InjectRepository(Token)
+    private readonly tokenRepository: Repository<Token>,
+    private readonly configService: ConfigService
   ) {}
-  async signUpLocal(_signUpLocalDto: SignUpLocalDto) {
-    this.userRepository; // prevent unused vars ts err
+
+  async signUpLocal({ username, password, email }: SignUpLocalDto): Promise<TokensInterface> {
+    const passwordHash = await hash(password, {
+      secret: Buffer.from(this.configService.get<string>("HASH_SECRET")!, "utf-8"),
+    });
+    const newUser = new User(email, passwordHash, username);
+    await this.userRepository.save(newUser);
+
+    const accessToken = await this.getAccessToken(newUser.id, newUser.email, newUser.type);
+    const refreshToken = await this.getRefreshToken(newUser.id, newUser.email);
+    const refreshTokenHash = await hash(refreshToken, {
+      secret: Buffer.from(this.configService.get<string>("HASH_SECRET")!, "utf-8"),
+    });
+    const refreshTokenObject = new Token(refreshTokenHash);
+    await this.tokenRepository.save(refreshTokenObject);
+    newUser.token = refreshTokenObject;
+    await this.userRepository.save(newUser);
+    return {
+      accessToken,
+      refreshToken,
+    };
   }
   async signInLocal(_signInLocalDto: SignInLocalDto) {}
   async logout() {}
   async refreshToken() {}
+
+  private async getAccessToken(userId: number, email: string, userType: UserTypes) {
+    const accessToken = await this.jwtService.signAsync(
+      {
+        sub: userId,
+        email,
+        userType,
+      },
+      {
+        expiresIn: this.configService.get<string>("ACCESS_TOKEN_TTL")!,
+        secret: this.configService.get<string>("ACCESS_TOKEN_SECRET")!,
+      }
+    );
+    return accessToken;
+  }
+  private async getRefreshToken(userId: number, email: string) {
+    const refreshToken = await this.jwtService.signAsync(
+      {
+        sub: userId,
+        email,
+      },
+      {
+        expiresIn: this.configService.get<string>("REFRESH_TOKEN_TTL")!,
+        secret: this.configService.get<string>("REFRESH_TOKEN_SECRET")!,
+      }
+    );
+    return refreshToken;
+  }
 }
