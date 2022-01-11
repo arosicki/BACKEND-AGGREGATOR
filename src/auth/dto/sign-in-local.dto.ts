c243ea9836ebac727a92049ba5616ae6c58787e6
Adrian Rosicki <adrian.rosicki.9@gmail.com>
2022-01-11 17:50:45 +0100

    remove unused import

--- src/auth/dto/sign-in-local.dto.ts
+++ src/auth/dto/sign-in-local.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsEmail, IsString, MaxLength, MinLength } from "class-validator";
+import { IsEmail, IsString, MaxLength } from "class-validator";
 
 export class SignInLocalDto {
   @IsString()
