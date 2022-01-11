0cd37c5b436c4590fb32fada45d52ca0c2281509
Adrian Rosicki <adrian.rosicki.9@gmail.com>
2022-01-11 17:38:31 +0100

    document dtos in swagger

--- src/auth/dto/sign-in-local.dto.ts
+++ src/auth/dto/sign-in-local.dto.ts
@@ -1,12 +1,22 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsString, MaxLength, MinLength } from "class-validator";
 
 export class SignInLocalDto {
   @IsString()
-  @MinLength(6)
   @MaxLength(32)
   @IsEmail()
+  @ApiProperty({
+    example: "zaq1@wsx.com",
+    format: "email",
+    maxLength: 32,
+  })
   email: string;
   @IsString()
   @MaxLength(32)
+  @ApiProperty({
+    example: "Adrian",
+    maxLength: 16,
+    minLength: 3,
+  })
   password: string;
 }
--- src/auth/dto/sign-up-local.dto.ts
+++ src/auth/dto/sign-up-local.dto.ts
@@ -1,19 +1,36 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsString, Matches, MaxLength, MinLength } from "class-validator";
 import { passwordRequirementsConfig } from "src/config/password-requirements-config";
 import { IsEmailAlreadyUsed } from "../decorators";
 
 export class SignUpLocalDto {
   @IsString()
-  @MinLength(6)
   @MaxLength(32)
   @IsEmail()
   @IsEmailAlreadyUsed()
+  @ApiProperty({
+    example: "zaq1@wsx.com",
+    format: "email",
+    maxLength: 32,
+  })
   email: string;
   @IsString()
+  @MaxLength(32)
   @Matches(passwordRequirementsConfig.getRegex(), { message: passwordRequirementsConfig.getDescription() })
+  @ApiProperty({
+    example: "zaq1@WSX",
+    format: "password",
+    maxLength: 32,
+    description: "password requirements may vary depending on config",
+  })
   password: string;
   @IsString()
   @MinLength(3)
   @MaxLength(16)
+  @ApiProperty({
+    example: "Adrian",
+    maxLength: 16,
+    minLength: 3,
+  })
   username: string;
 }
