fbb315b82dbc2d4f9e5b34d07fceed89a19ed416
Adrian Rosicki <adrian.rosicki.9@gmail.com>
2022-01-12 13:57:56 +0100

    remove unnecessary comment

--- src/auth/entities/token.entity.ts
+++ src/auth/entities/token.entity.ts
@@ -1,8 +1,6 @@
 import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { User } from ".";
 
-/* Created to prevent changing up modified every time new refresh token is generated */
-
 @Entity("tokens")
 export class Token {
   constructor(token: string) {
