c9bcbb635ffa1364dc42fd9bae831ff7a0fa1076
Adrian Rosicki <adrian.rosicki.9@gmail.com>
2022-01-12 16:29:30 +0100

    add cookie parser

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import cookieParser from "cookie-parser";
 import { AppModule } from "./app.module";
 import { validationPipeConfig } from "./config/validation-pipe-config";
 
@@ -20,5 +21,6 @@ import { validationPipeConfig } from "./config/validation-pipe-config";
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
   const configService = app.get(ConfigService);
+  app.use(cookieParser(configService.get("COOKIE_SECRET")!));
   await app.listen(configService.get("PORT") as number);
 })();
